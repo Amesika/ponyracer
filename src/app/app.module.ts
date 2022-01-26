a82dc1eb1b05496b745f1680fc804eda8542d08b
Amesika <am3sika@gmail.com>
2022-01-26 23:20:11 +0100

    10. HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -7,9 +8,10 @@ import { RacesComponent } from './races/races.component';
 import { RaceComponent } from './race/race.component';
 import { PonyComponent } from './pony/pony.component';
 
+
 @NgModule({
   declarations: [AppComponent, MenuComponent, RacesComponent, RaceComponent, PonyComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
