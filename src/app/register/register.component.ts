78e185542c98550efdda73ea30248fa2540bd4ab
Amesika <am3sika@gmail.com>
2022-01-27 14:00:05 +0100

    15. Custom validators in forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'pr-register',
@@ -10,23 +12,49 @@ export class RegisterComponent implements OnInit {
 
   loginCtrl: FormControl
   passwordCtrl: FormControl
+  confirmPasswordCtrl: FormControl;
   birthYearCtrl: FormControl
   userForm: FormGroup
+  passwordGroup: FormGroup;
+  registrationFailed= false;
 
-  constructor(fb:FormBuilder) {
-    this.loginCtrl = fb.control('',Validators.required)
-    this.passwordCtrl = fb.control('',Validators.required)
-    this.birthYearCtrl = fb.control('',Validators.required)
-    this.userForm= fb.group({
+  constructor(fb: FormBuilder, private userService:UserService, private router:Router) {
+    this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)])
+    this.passwordCtrl = fb.control('', Validators.required)
+    this.birthYearCtrl = fb.control('', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]);
+    this.confirmPasswordCtrl = fb.control('', Validators.required);
+    this.passwordGroup = fb.group(
+      {
+        password: this.passwordCtrl,
+        confirmPassword: this.confirmPasswordCtrl
+      },
+      {
+        validators: RegisterComponent.passwordMatch
+      }
+    );
+    this.userForm = fb.group({
       login: this.loginCtrl,
-      password : this.passwordCtrl,
-      birthYear : this.birthYearCtrl
+      passwordForm: this.passwordGroup,
+      birthYear: this.birthYearCtrl
     })
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  register(): void {}
+  register(): void {
+    this.userService
+      .register(this.userForm.value.login, this.userForm.value.passwordForm.password, this.userForm.value.birthYear)
+      .subscribe({
+        next: () => this.router.navigate(['/']),
+        error: () => (this.registrationFailed = true)
+      });
+  }
+
+  static passwordMatch(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')!.value;
+    const confirmPassword = control.get('confirmPassword')!.value;
+    return password !== confirmPassword ? { matchingError: true } : null;
+  }
 
 }
